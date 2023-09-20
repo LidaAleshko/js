67a9cd96147575ade8875a05d7f64986a0965620
LidaAleshko <144565161+LidaAleshko@users.noreply.github.com>
2023-09-21 00:51:12 +0300

    dzz11

---
+++ dz11/js/script.js
@@ -0,0 +1,190 @@
+//1
+
+let str = "Привет";
+let sum = 123;
+let num = 15.8;
+let flag = true; 
+let txt = "true";
+console.log(typeof "Привет");
+console.log(typeof 123);
+console.log(typeof 15.8);
+console.log(typeof true);
+console.log(typeof "true");
+console.log(str > sum);
+console.log(sum > num);
+console.log(num < flag);
+console.log(flag == txt);
+console.log(flag >= txt);
+console.log(flag <= txt);
+console.log(flag != txt);
+let a1 = 5 % 3,
+    a2 = 3 % 5,
+    a3 = 5 + '3',
+    a4 = '5' - 3,
+    a5 = 75 + 'кг',
+    a6 = 785 * 653,
+    a7 = 100 / 25,
+    a8 = 0 * 0,
+    a9 = 0 / 2,
+    a10 = 89 / 0,
+    a11 = 98 + 2,
+    a12 = 5 - 98,
+    a13 = (8 + 56 * 4) / 5,
+    a14 = (9 - 12) * 7 / (5 + 2),
+    a15 = +"123",
+    a16 = 1 || 0,
+    a17 = false || true,
+    a18 = true > 0; 
+
+//2
+
+console.log(a1)
+console.log(a2)
+console.log(a3)
+console.log(a4)
+console.log(a5)
+console.log(a6)
+console.log(a7)
+console.log(a8)
+console.log(a9)
+console.log(a10)
+console.log(a11)
+console.log(a12)
+console.log(a13)
+console.log(a14)
+console.log(a15)
+console.log(a16)
+console.log(a17)
+console.log(a18)
+
+console.log(typeof a2)
+console.log(typeof a3)
+console.log(typeof a4)
+console.log(typeof a5)
+console.log(typeof a6)
+console.log(typeof a7)
+console.log(typeof a8)
+console.log(typeof a9)
+console.log(typeof a10)
+console.log(typeof a11)
+console.log(typeof a12)
+console.log(typeof a13)
+console.log(typeof a14)
+console.log(typeof a15)
+console.log(typeof a16)
+console.log(typeof a17)
+console.log(typeof a18)
+
+//3
+//s=ab
+
+let height = 23;
+let width = 10;
+let SPryam = (height * width);
+console.log(SPryam, 'см2');
+
+//4
+//v=π(r*r)h 
+
+let h = 10;
+let d = a7;
+let π = 3.14;
+let VCilindra = (π * (d * d) * h);
+console.log(VCilindra, 'м3')
+
+
+//5
+//s=π(r*r)
+
+let r = 5;
+let SKruga = π * (r*r)
+console.log(SKruga, 'см2')
+
+
+//6
+//s=((a+b)/2)h
+
+let a = 5;
+let b = 7;
+let STrap = ((a + b)/2) * h;
+console.log(STrap, 'см2');
+
+//7
+//Pereplata=((S+(S/100)10)*5)-(s*5)
+
+let S = 2000000;
+let p = 10;
+let years = 5;
+let Pereplata = ((S + (S / 100)*p) * years) - (S * years);
+console.log(Pereplata, 'млн. руб');
+
+
+//8
+//a + (2 * (x - b)) = 16;
+
+let a19 = 8;
+let b19 = 3;
+x1 = (16 - a19) / 2 + b19;
+console.log(x1); 
+
+//b(x+15)=a+6x;
+
+let a20 = 8;
+let b20 = 3;
+x2 = 37 / 3;
+console.log(x2)
+
+//x+2x+ax+bx=23780
+
+let a35 = 8;
+let b35 = 3;
+x3 = 23780 / 14;
+console.log(x3);
+
+//9
+let a21 = 'Бывало, спит у ног собака,';
+console.log(a21);
+let a22 = 'костер занявшийся гудит,';
+console.log(a22);
+let a23 = 'и женщина из полумрака';
+console.log(a23);
+let a24 = 'глазами зыбкими глядит.';
+console.log(a24);
+let a29 = '';
+console.log(a29);
+console.log(a29);
+let a25 = 'Потом под пихтою приляжет';
+console.log(a25);
+let a26 ='на куртку рыжую мою';
+console.log(a26);
+let a27 = 'и мне, задумчивая, скажет:';
+console.log(a27);
+let a30 = '';
+console.log(a30);
+console.log(a30);
+let a28 = '«А ну-ка, спой!..»- и я пою.';
+console.log(a28);
+let a36 = "";
+console.log(a36);
+
+//10
+let text1 = "индо земля зашаталась под ногами-и вырос,";
+let text2 = "и заревел он голосом диким...";
+let text3 = "блеснула молния и ударил гром,";
+let text4 = "а так какое-то чудище, страшное и мохнатое,";
+let text5 = "как будто из-под земли, перед купцом:";
+let text6 = "Он подошёл и сорвал аленький цветочек.";
+let text7 = "зверь не зверь, человек не человек,";
+let text8 = "В ту же минуту, безо всяких туч,";
+console.log(text6, text8, text3, text1, text5, text7, text4, text2);
+
+
+
+
+
+
+
+
+
+
+
